fbeb0f9abfe113c943e2802f5921d6b427a7ff3b
Dulangi2002 <cb010550@students.apiit.lk>
2023-09-15 11:05:41 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Nore-App/sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Note-App/sw.js', { scope: './' })})}

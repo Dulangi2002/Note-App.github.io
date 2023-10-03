7967d9d5e4827b73fa0a53ed460d3ed7624d517d
Dulangi2002 <cb010550@students.apiit.lk>
2023-10-03 11:29:07 +0530

    styling

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -22,29 +22,35 @@ function SignIn() {
             })
             .catch((error) => {
                 console.log(error);
-            
+
             });
 
     }
     return (
-        <div>
-            <div className="form-control mb-24 w-48 mt-56  ">
+        <div class="w-full max-w-xs" >
+            <div className=" ">
 
-                <form action="" className='bg-purple-700 opacity-50 '>
+                <form action="" className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
                     <div className='mb-4'>
-                    <label htmlFor="email" className=" text-gray-700 text-sm font-bold mb-2">Email</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700  focus:outline-none focus:shadow-outline  " type="text" name="email" id="" onChange={(e) => setEmail(e.target.value)} />
-                    <br />
+                        <label htmlFor="email" className=" text-gray-700 text-sm font-bold mb-2">Email</label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700  focus:outline-none focus:shadow-outline  " type="text" name="email" id="" onChange={(e) => setEmail(e.target.value)} />
+                        <br />
                     </div>
 
                     <div>
 
-                    <label htmlFor="password" className=' text-gray-700 text-sm font-bold mb-2'>Password</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline " type="text" name="password" id="" onChange={(e) => setPassword(e.target.value)} />
+                        <label htmlFor="password" className=' text-gray-700 text-sm font-bold mb-2'>Password</label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline " type="text" name="password" id="" onChange={(e) => setPassword(e.target.value)} />
                     </div>
 
                     <br />
-                    <button onClick={handleSignIn}>Signin</button>
+                    <div class="flex items-center justify-between">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleSignIn}>Signin</button>
+
+                        <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                            Forgot Password?
+                        </a>
+                    </div>
                 </form>
             </div>
         </div>

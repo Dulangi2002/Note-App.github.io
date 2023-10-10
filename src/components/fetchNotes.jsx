4383cae4c7c982975d95feb2fb400263a1866d24
Dulangi2002 <cb010550@students.apiit.lk>
2023-10-10 11:46:53 +0530

    styling

--- src/components/fetchNotes.jsx
+++ src/components/fetchNotes.jsx
@@ -29,12 +29,31 @@ function FetchNotes({ note }) {
   const [categoryFilter, setCategoryFilter] = useState([]);
   const [isExpanded, setIsExpanded] = useState(false);
   const [isZoomed, setIsZoomed] = useState(false);
+ 
+  const [expandedNotesForImages, setExpandedNotesForImages] = useState([]);
+  const [index, setIndex] = useState(0);
+
+
+
 
   const navigate = useNavigate();
 
+
+  const handleLoadMoreImages = (index) => {
+    const updatedexpandedNotesForImages = [...expandedNotesForImages, index];
+    setExpandedNotesForImages(updatedexpandedNotesForImages);
+  };
+  
+  const handleShowLessImages = (index) => {
+    const updatedexpandedNotesForImages = expandedNotesForImages.filter((item) => item !== index);
+    setExpandedNotesForImages(updatedexpandedNotesForImages);
+  };
+  
+
+
   const handleExpand = () => {
     setIsExpanded(!isExpanded);
-  };  
+  };
 
 
   const handleZoom = () => {
@@ -211,156 +230,173 @@ function FetchNotes({ note }) {
 
   return (
 
-    <div id='whole'>
+    <div >
 
       <CreateCategory />
 
       {/* create filter for the categories */}
 
 
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
 
 
-      <div className='filter' id='filter'>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
+        <div  >
+
+          <select onChange={(e) => setSelectedCategory(e.target.value)}>
+
+            <option value="all">All</option>
+            {categories.map((category) => (
+              <option value={category.category_name}>{category.category_name}</option>
+            ))}
 
 
-          <div id='note-blocks'>
-
-            <select onChange={(e) => setSelectedCategory(e.target.value)}>
-
-              <option value="all">All</option>
-              {categories.map((category) => (
-                <option value={category.category_name}>{category.category_name}</option>
-              ))}
+          </select>
 
 
-            </select>
-
-            <div>
-              <div className='flex  flex-row'>
-                <div className=' font-bold ml-8 '>
-                  <h1 id='notes-heading' className='text-5xl'> Your Notes</h1>
-                </div> <div className='flex lg:flex-col mt-8 gap-4 ml-2 sm:rounded-lg '>
-                  <div >
-                    <button onClick={goToTasks} id='submenu-buttons'>View your tasks</button>
-                  </div>
-
-                  <div> <button onClick={getCreateNoteForm} id='submenu-buttons'>create new note</button></div>
-                </div>
+          <div className=''>
+            <div className=' font-bold  '>
+              <h1 id='notes-heading' className='text-5xl ml-4 '> Your Notes</h1>
+            </div>
+            <div className='flex  mt-8 gap-4 ml-2 sm:rounded-lg '>
+              <div >
+                <button onClick={goToTasks} id='submenu-buttons' className='text-white '> View your tasks</button>
               </div>
-              {
-                filterNotesByCategory.map((note) => (
+
+              <div> <button onClick={getCreateNoteForm} id='submenu-buttons'>create new note</button></div>
+            </div>
+          </div>
+
+          <div className='grid grid-cols-1 lg:grid-cols-4 lg:ml-20 '>
 
 
-                  <div id='single-note-block'
-                    style={{
-                      backgroundColor: note.colorLabel,
 
-                    }}>
-                    <div
-                      className=' '
-                      id='label-block'
-                      style={{
-                        backgroundColor: note.colorLabel,
-                      }}
-                    >
-                    </div>
-                    <p className="card-text mt-8 " >{note.createdAt.toDate().toLocaleString()}</p>
-
-                    <div className="" key={note.id}>
-                      <div className="card-body">
-                        <h5 className="card-title font-[DM sans] text-xl" contentEditable="true" suppressContentEditableWarning onInput={
-                          (event) => {
-                            const newTitle = event.target.innerText;
-                            console.log(newTitle);
-                            const docRef = doc(firestore, 'users', userEmail, 'notes', note.id);
-                            updateDoc(docRef, {
-                              title: newTitle
-                            })
-                          }
-                        }>{note.title}</h5>
-
-                        <div className='flex flex-row '>
-                          <div>
-                            <button onClick={deleteNote.bind(this, note.id)} className="" id='delete-button'>
-                              <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
-                                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
-                              </svg>   </button>
-                          </div>
-
-                          <div>
-
-                            <button onClick={
-                              () => {
-                                handleshareButtonClick(note);
-
-                              }
+            {
+              filterNotesByCategory.map((note) => (
 
 
-                            } id='share-button'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
-                                <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
-                              </svg>  </button>
-                            <p>{result}</p>
-                          </div>
+                <div id='' className=' p-4  lg:w-80 w-full mr-2  mb-8 mt-8 h-auto text-black lg:rounded  border-t-2  border-b-2 border-gray-300'
+                  style={{
+                    backgroundColor: note.colorLabel,
 
+                  }}>
 
-                        </div>
+                  <p>
+                    {note.category}
+                  </p>
+                  <p className="card-text mt-8 " >{note.createdAt.toDate().toLocaleString()}</p>
 
-                        <div className="your-container-div">
-                          <p className={`card-text font-[Nunito] text-xl ${isExpanded ? '' : 'truncate'}`} contentEditable="true" suppressContentEditableWarning
-                            onInput={(event) => {
-                              const newContent = event.target.innerText;
-                              console.log(newContent);
-                              const docRef = doc(firestore, 'users', userEmail, 'notes', note.id);
-                              updateDoc(docRef, {
-                                content: newContent
-                              });
-                            }}
-                          >
-                            {isExpanded ? note.content : note.content.substring(0, 100) + '...'}
-                            {!isExpanded && <span onClick={handleExpand} id='expand-button'>Load more</span>}
-                          </p>
-                        
-                        </div>
+                  <div className="" key={note.id}>
+                    <div className="card-body">
+                      <h5 className="card-title font-[DM sans] text-xl" contentEditable="true" suppressContentEditableWarning onInput={
+                        (event) => {
+                          const newTitle = event.target.innerText;
+                          console.log(newTitle);
+                          const docRef = doc(firestore, 'users', userEmail, 'notes', note.id);
+                          updateDoc(docRef, {
+                            title: newTitle
+                          })
+                        }
+                      }>{note.title}</h5>
+
+                      <div className='flex flex-row '>
                         <div>
-                          <button onClick={handleExpand} id='expand-button'>load more</button>
+                          <button onClick={deleteNote.bind(this, note.id)} className="" id='delete-button'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+                              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+                            </svg>   </button>
                         </div>
+
                         <div>
-                          <h2> Images:</h2>
-                          {Array.isArray(note.files) && note.files.map((file, index) => {
 
-                            return <img key={index} src={file} alt={`Image ${index}`} className='h-56 w-56 border rounded  object-cover' id='Zoom-image' />;
-                          })}
+                          <button onClick={
+                            () => {
+                              handleshareButtonClick(note);
+
+                            }
+
+
+                          } id='share-button'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
+                              <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
+                            </svg>  </button>
+                          <p>{result}</p>
                         </div>
-
-                        <p>
-                          {note.category}
-                        </p>
-
-
-
-
 
 
                       </div>
+
+                      <div className="your-container-div">
+                        <p className={`card-text font-[Nunito] text-xl ${isExpanded ? '' : 'truncate'}`} contentEditable="true" suppressContentEditableWarning
+                          onInput={(event) => {
+                            const newContent = event.target.innerText;
+                            console.log(newContent);
+                            const docRef = doc(firestore, 'users', userEmail, 'notes', note.id);
+                            updateDoc(docRef, {
+                              content: newContent
+                            });
+                          }}
+                        >
+                          {isExpanded ? note.content : note.content.substring(0, 100) + '...'}
+                          {!isExpanded && <span onClick={handleExpand} id='expand-button'>Load more</span>}
+                        </p>
+
+                      </div>
+                      <div>
+                        <button onClick={handleExpand} id='expand-button'>load more</button>
+                      </div>
+
+                      
+                      <div>
+    
+      {Array.isArray(note.files) &&
+  note.files.slice(0, expandedNotesForImages.includes(index) ? note.files.length : 1).map((file, imgIndex) => (
+    <img
+      key={imgIndex}
+      src={file}
+      alt={`Image ${imgIndex}`}
+      className='h-56 w-56 border rounded object-cover'
+      id='Zoom-image'
+    />
+  ))
+}
+
+{note.files.length > 1 && (
+  <button onClick={() => handleLoadMoreImages(index)} className='text-white'>
+    Load more images
+  </button>
+)}
+
+{expandedNotesForImages.includes(index) && note.files.length > 1 && (
+  <button onClick={() => handleShowLessImages(index)} className='text-white'>
+    Show less images
+  </button>
+)}
+
+    </div>
+
+
+
+
+
+
                     </div>
                   </div>
+                </div>
 
-                ))}
-
-
-            </div>
-
-
-
+              ))}
 
           </div>
-        )}
 
-      </div>
+
+
+
+
+        </div>
+      )}
+
+
     </div>
   );
 }

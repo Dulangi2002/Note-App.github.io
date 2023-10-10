885ee9658f307f1dffcb346f47dd8bad0bdea306
Dulangi2002 <cb010550@students.apiit.lk>
2023-10-10 18:00:37 +0530

    styling

--- src/components/viewProfile.jsx
+++ src/components/viewProfile.jsx
@@ -88,7 +88,7 @@ function ViewProfile({ userEmail, firestore }) {
 
 
 
-  
+
 
     const navigateToProfilePhotoPage = () => {
         navigate('/Note-App/profilePhoto');
@@ -156,9 +156,9 @@ function ViewProfile({ userEmail, firestore }) {
 
     return (
         <div>
-            <h1>View Profile</h1>
 
-            <div className='flex flex-col' id='image-container'>
+
+            <div className='flex flex-col' id=''>
                 <div>
                     {profilePhoto ? (
                         <img src={profilePhoto} alt="Profile" className=' ' id='profile-image' />
@@ -184,28 +184,56 @@ function ViewProfile({ userEmail, firestore }) {
 
 
 
-            <div>
-                <h2 className=" text-2xl ">{currentUser?.displayName}</h2>
-                <p>{currentUser?.email}</p>
-                <button
-                    onClick={() => setIsEditEmailFormVisible(!isEditEmailFormVisible)}
+            <div id='profile-container flex flex-row ' >
 
-                >
-                    Edit Email
+                <div className='flex flex-row space-between border m-2 '>
+                    <div className='text-xl font-bold font-[Nunito] justify-center m-4 ' >
+                        <p >{currentUser?.email}</p>
 
-                </button>
-                {isEditEmailFormVisible && <EditEmailForm email={currentUser?.email} />}
-                <img src={currentUser?.photoURL} alt="user profile" />
-                <img src="" alt="" />
+                    </div>
 
-                <button onClick={deleteAccount} >
-                    Delete account
-                </button>
+                    <div>
+                        <button
+                            onClick={() => setIsEditEmailFormVisible(!isEditEmailFormVisible)}
+                            className='w-20 mt-8 '
+
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
+                            </svg>
+                        </button>
+                        {isEditEmailFormVisible && <EditEmailForm email={currentUser?.email} />}
+
+                    </div>
+
+                </div>
 
 
-                <button onClick={deleteNotes}>
-                    Delete Notes
-                </button>
+
+
+
+
+                <div className='bg-red-500  h-14 m-2 text-center text-xl text-white pt-2 '>
+
+
+                    <button onClick={deleteAccount}  >
+                        Delete My Account
+                    </button>
+                </div>
+
+                <div className='bg-red-500  h-14 m-2 text-center text-xl text-white pt-2 '>
+
+                    <button onClick={deleteNotes}>
+                        Delete My Notes
+                    </button>
+                </div>
+
+
+
+
+
+
+
 
 
             </div>
